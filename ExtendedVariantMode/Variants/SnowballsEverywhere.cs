e96f5183a56f05edbf2dc4f003b1fdfc0f00de29
max480 <max480@free.fr>
2019-12-01 10:57:34 +0100

    Add delay to first snowball to prevent softlocks

--- ExtendedVariantMode/Variants/SnowballsEverywhere.cs
+++ ExtendedVariantMode/Variants/SnowballsEverywhere.cs
@@ -26,12 +26,14 @@ namespace ExtendedVariants.Variants {
             On.Celeste.Level.LoadLevel += modLoadLevel;
             On.Celeste.Level.TransitionRoutine += modTransitionRoutine;
             IL.Celeste.Snowball.Update += modSnowballUpdate;
+            IL.Celeste.Snowball.Added += modSnowballAdded;
         }
 
         public override void Unload() {
             On.Celeste.Level.LoadLevel -= modLoadLevel;
             On.Celeste.Level.TransitionRoutine -= modTransitionRoutine;
             IL.Celeste.Snowball.Update -= modSnowballUpdate;
+            IL.Celeste.Snowball.Added -= modSnowballAdded;
         }
 
         private void modLoadLevel(On.Celeste.Level.orig_LoadLevel orig, Level self, Player.IntroTypes playerIntro, bool isFromLoader) {
@@ -64,6 +66,26 @@ namespace ExtendedVariants.Variants {
                 // send the snowball off-screen, so that the player gets 0.8 seconds before the first snowball
                 snowball.X = level.Camera.Left - 60f;
             }
+        }
+
+        private void modSnowballAdded(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            // jump right after ResetPosition() is called
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchCallvirt<Snowball>("ResetPosition"))) {
+                Logger.Log("ExtendedVariantMode/SnowballsEverywhere", $"Modding initial snowball delay at {cursor.Index} in CIL code for Added in Snowball");
+
+                // and substitute the resetTimer with our own (ResetPosition sets it to 0)
+                cursor.Emit(OpCodes.Ldarg_0);
+                cursor.EmitDelegate<Func<float>>(determineInitialResetTimer);
+                cursor.Emit(OpCodes.Stfld, typeof(Snowball).GetField("resetTimer", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance));
+            }
+        }
+
+        private float determineInitialResetTimer() {
+            // we want the first snowball to be issued with a minimum delay of 0.8 seconds whatever the setting to avoid softlocks.
+            // to do that, we will initialize the resetTimer to -0.5f if the snowball delay is of 0.3 seconds for example.
+            return Math.Min(0, Settings.SnowballDelay / 10f - 0.8f);
         }
 
         private void modSnowballUpdate(ILContext il) {
