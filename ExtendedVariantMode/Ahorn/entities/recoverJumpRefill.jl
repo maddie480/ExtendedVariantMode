8aaf71e268279679a91b593d221aac5b99f7452d
max480 <max480@free.fr>
2020-03-25 09:13:20 +0100

    Ahorn plugin: Support for rendering legacy recover jump refills

--- ExtendedVariantMode/Ahorn/entities/recoverJumpRefill.jl
+++ ExtendedVariantMode/Ahorn/entities/recoverJumpRefill.jl
@@ -3,6 +3,9 @@
 using ..Ahorn, Maple
 
 @mapdef Entity "ExtendedVariantMode/RecoverJumpRefill" RecoverJumpRefill(x::Integer, y::Integer, oneUse::Bool=false, texture::String="ExtendedVariantMode/jumprefill")
+@mapdef Entity "ExtendedVariantMode/JumpRefill" LegacyJumpRefill(x::Integer, y::Integer, oneUse::Bool=false, texture::String="ExtendedVariantMode/jumprefill")
+
+recoverJumpRefills = Union{RecoverJumpRefill, LegacyJumpRefill}
 
 const placements = Ahorn.PlacementDict(
     "Recover Jump Refill (Extended Variant Mode)" => Ahorn.EntityPlacement(
@@ -10,13 +13,13 @@ const placements = Ahorn.PlacementDict(
     )
 )
 
-function Ahorn.selection(entity::RecoverJumpRefill)
+function Ahorn.selection(entity::recoverJumpRefills)
     sprite = "objects/" * get(entity.data, "texture", "ExtendedVariantMode/jumprefill") * "/idle00"
     x, y = Ahorn.position(entity)
     return Ahorn.getSpriteRectangle(sprite, x, y)
 end
 
-function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::RecoverJumpRefill, room::Maple.Room)
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::recoverJumpRefills, room::Maple.Room)
     sprite = "objects/" * get(entity.data, "texture", "ExtendedVariantMode/jumprefill") * "/idle00"
     Ahorn.drawSprite(ctx, sprite, 0, 0)
 end
