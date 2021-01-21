f2e64cdad12609d752d503ab1e238a13b108aa00
max4805 <max480@free.fr>
2021-01-21 23:05:07 +0100

    Support enabling Held Dash through DynData attribute on Player

--- ExtendedVariantMode/Variants/HeldDash.cs
+++ ExtendedVariantMode/Variants/HeldDash.cs
@@ -2,6 +2,7 @@
 using Celeste.Mod;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using MonoMod.Utils;
 using System;
 using System.Collections;
 
@@ -37,7 +38,7 @@ namespace ExtendedVariants.Variants {
                 object o = coroutine.Current;
                 if (o != null && o.GetType() == typeof(float)) {
                     yield return o;
-                    while (Settings.HeldDash && Input.Dash.Check) {
+                    while (Input.Dash.Check && hasHeldDash(self)) {
                         yield return null;
                     }
                 } else {
@@ -62,15 +63,21 @@ namespace ExtendedVariants.Variants {
             cursor.Emit(OpCodes.Ldarg_0);
             cursor.Emit(OpCodes.Ldarg_0);
             cursor.Emit(OpCodes.Ldfld, typeof(Player).GetField("dashTrailCounter", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance));
-            cursor.EmitDelegate<Func<int, int>>(modDashTrailCounter);
+            cursor.Emit(OpCodes.Ldarg_0);
+            cursor.EmitDelegate<Func<int, Player, int>>(modDashTrailCounter);
             cursor.Emit(OpCodes.Stfld, typeof(Player).GetField("dashTrailCounter", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance));
         }
 
-        private int modDashTrailCounter(int dashTrailCounter) {
-            if (Settings.HeldDash) {
+        private int modDashTrailCounter(int dashTrailCounter, Player self) {
+            if (hasHeldDash(self)) {
                 return 2; // lock the counter to 2 to have an infinite trail
             }
             return dashTrailCounter;
+        }
+
+        private bool hasHeldDash(Player self) {
+            // expose an "ExtendedVariantsHeldDash" DynData field to other mods.
+            return Settings.HeldDash || (new DynData<Player>(self).Data.TryGetValue("ExtendedVariantsHeldDash", out object o) && o is bool b && b);
         }
     }
 }
