6c3124482a247362a3ceb23044adbe040090b07e
max4805 <max480@free.fr>
2021-05-25 19:44:02 +0200

    Add EveryJumpIsUltra to dropdown in Ahorn

--- ExtendedVariantMode/Ahorn/libraries/extendedVariantDictionary.jl
+++ ExtendedVariantMode/Ahorn/libraries/extendedVariantDictionary.jl
@@ -43,6 +43,7 @@ const Variants = String[
 	"DisplayDashCount",
 	"DontRefillDashOnGround",
 	"DontRefillStaminaOnGround",
+	"EveryJumpIsUltra",
 	"EverythingIsUnderwater",
 	"ExplodeLaunchSpeed",
 	"FallSpeed",
