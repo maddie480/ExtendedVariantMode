d1b93918044f19620dafe9b4da374f6e915d79a3
max4805 <max480@free.fr>
2021-10-31 17:10:46 +0100

    Speedometer: Show top speed for last 10 frames

--- ExtendedVariantMode/Entities/Speedometer.cs
+++ ExtendedVariantMode/Entities/Speedometer.cs
@@ -1,8 +1,12 @@
 ﻿using Celeste;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ExtendedVariants.Entities {
     class Speedometer : DashCountIndicator {
+        private LinkedList<int> lastSpeeds = new LinkedList<int>();
+
         protected override bool shouldShowCounter() {
             return settings.DisplaySpeedometer != 0;
         }
@@ -12,16 +16,28 @@ namespace ExtendedVariants.Entities {
         }
 
         protected override string getNumberToDisplay(Player player) {
+            int mostRecentNumber;
             switch (settings.DisplaySpeedometer) {
                 case 1:
-                    return ((int) Math.Abs(Math.Round(player.Speed.X))).ToString();
+                    mostRecentNumber = (int) Math.Abs(Math.Round(player.Speed.X));
+                    break;
                 case 2:
-                    return ((int) Math.Abs(Math.Round(player.Speed.Y))).ToString();
+                    mostRecentNumber = (int) Math.Abs(Math.Round(player.Speed.Y));
+                    break;
                 case 3:
-                    return ((int) Math.Round(player.Speed.Length())).ToString();
+                    mostRecentNumber = (int) Math.Round(player.Speed.Length());
+                    break;
                 default:
-                    return "";
+                    mostRecentNumber = 0;
+                    break;
             }
+
+            // we're displaying the top speed from the last 10 frames.
+            lastSpeeds.AddLast(mostRecentNumber);
+            if (lastSpeeds.Count > 10) {
+                lastSpeeds.RemoveFirst();
+            }
+            return lastSpeeds.Max().ToString();
         }
     }
 }
