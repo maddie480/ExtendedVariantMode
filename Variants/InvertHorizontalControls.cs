d64a36ce5acaaaecc32cb3e63c9c64325975df0c
nhruo <nhruo123@gmail.com>
2023-09-21 12:11:15 +0300

    fix MirrorMode reversing controlls

--- Variants/InvertHorizontalControls.cs
+++ Variants/InvertHorizontalControls.cs
@@ -1,43 +1,58 @@
 ﻿using Celeste;
 using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
 using System;
 using static ExtendedVariants.Module.ExtendedVariantsModule;
 
-namespace ExtendedVariants.Variants {
-    public class InvertHorizontalControls : AbstractExtendedVariant {
+namespace ExtendedVariants.Variants
+{
+    public class InvertHorizontalControls : AbstractExtendedVariant
+    {
 
-        public override Type GetVariantType() {
+        public override Type GetVariantType()
+        {
             return typeof(bool);
         }
 
-        public override object GetDefaultVariantValue() {
+        public override object GetDefaultVariantValue()
+        {
             return false;
         }
 
-        public override object ConvertLegacyVariantValue(int value) {
+        public override object ConvertLegacyVariantValue(int value)
+        {
             return value != 0;
         }
 
-        public override void Load() {
-            using (new DetourContext {
+        public override void Load()
+        {
+            using (new DetourContext
+            {
                 After = { "*" } // we want to be extra sure we're applied after Crow Control here.
-            }) {
+            })
+            {
                 On.Celeste.Level.Update += onLevelUpdate;
             }
         }
 
-        public override void Unload() {
+        public override void Unload()
+        {
             On.Celeste.Level.Update -= onLevelUpdate;
         }
 
-        private void onLevelUpdate(On.Celeste.Level.orig_Update orig, Level self) {
-            if (Input.Aim == null || Input.MoveX == null || SaveData.Instance?.Assists == null) {
+        private void onLevelUpdate(On.Celeste.Level.orig_Update orig, Level self)
+        {
+            if (Input.Aim == null || Input.MoveX == null || SaveData.Instance?.Assists == null)
+            {
                 orig(self);
                 return;
             }
 
+            bool isMirrorModeActive = GetVariantValue<bool>(Variant.MirrorMode) || SaveData.Instance.Assists.MirrorMode;
+            bool isInvertHorizontalControlsActive = GetVariantValue<bool>(Variant.InvertHorizontalControls);
+
             // this is vanilla behavior
-            Input.Aim.InvertedX = SaveData.Instance.Assists.MirrorMode;
+            Input.Aim.InvertedX = isMirrorModeActive;
 
             // there may be Crow Control here. if so, it will mess with Input.Aim.InvertedX
             orig(self);
@@ -45,8 +60,7 @@ namespace ExtendedVariants.Variants {
             // at this point, Input.Aim.InvertedX is either the vanilla value, or what Crow Control wants.
             // either way, we should keep it or invert it based on our settings.
 
-            bool expectedValue = Input.Aim.InvertedX;
-            if (GetVariantValue<bool>(Variant.InvertHorizontalControls)) expectedValue = !expectedValue;
+            bool expectedValue = Input.Aim.InvertedX ^ isInvertHorizontalControlsActive;
 
             Input.Aim.InvertedX = expectedValue;
             Input.MoveX.Inverted = expectedValue;
