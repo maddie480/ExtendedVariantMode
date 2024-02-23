a8788d6a75c3fd034b0ff45ec63b30a528b31de4
Dominic Aglialoro <domiaglialoro@gmail.com>
2024-02-22 16:38:41 -0800

    Better LiftBoost Protection implementation

--- Variants/LiftboostProtection.cs
+++ Variants/LiftboostProtection.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.Reflection;
 using Celeste;
+using Celeste.Mod;
 using ExtendedVariants.Module;
 using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using Platform = Celeste.Platform;
 
 namespace ExtendedVariants.Variants {
     public class LiftboostProtection : AbstractExtendedVariant {
+        private static bool TryGetPlatform(Player player, Vector2 dir, out Platform platform) {
+            if (dir.X == 0f && dir.Y > 0f)
+                platform = player.CollideFirst<Platform>(player.Position + dir);
+            else
+                platform = player.CollideFirst<Solid>(player.Position + dir);
+
+            return platform != null;
+        }
+    
+        private ILHook il_Celeste_Player_Orig_WallJump;
+    
         public override void Load() {
             On.Celeste.Player.Jump += Player_Jump;
             On.Celeste.Player.SuperJump += Player_SuperJump;
+            il_Celeste_Player_Orig_WallJump
+                = new ILHook(typeof(Player).GetMethod("orig_WallJump", BindingFlags.NonPublic | BindingFlags.Instance), Player_orig_WallJump_il);
             On.Celeste.Player.SuperWallJump += Player_SuperWallJump;
-            IL.Celeste.Platform.Update += Platform_Update_il;
+            On.Celeste.Platform.Update += Platform_Update;
             IL.Celeste.Platform.MoveH_float += PatchLiftboostProtectionX;
             IL.Celeste.Platform.MoveH_float_float += PatchLiftboostProtectionX;
             IL.Celeste.Platform.MoveV_float += PatchLiftboostProtectionY;
@@ -27,8 +45,9 @@ namespace ExtendedVariants.Variants {
         public override void Unload() {
             On.Celeste.Player.Jump -= Player_Jump;
             On.Celeste.Player.SuperJump -= Player_SuperJump;
+            il_Celeste_Player_Orig_WallJump.Dispose();
             On.Celeste.Player.SuperWallJump -= Player_SuperWallJump;
-            IL.Celeste.Platform.Update -= Platform_Update_il;
+            On.Celeste.Platform.Update -= Platform_Update;
             IL.Celeste.Platform.MoveH_float -= PatchLiftboostProtectionX;
             IL.Celeste.Platform.MoveH_float_float -= PatchLiftboostProtectionX;
             IL.Celeste.Platform.MoveV_float -= PatchLiftboostProtectionY;
@@ -46,57 +65,60 @@ namespace ExtendedVariants.Variants {
         public override object GetDefaultVariantValue() => false;
 
         public override object ConvertLegacyVariantValue(int value) => value != 0;
-
-        private static void CheckForLiftboost(Player player, Vector2 dir) {
-            if (player.LiftSpeed != Vector2.Zero)
-                return;
-
-            Platform platform;
-
-            if (dir.X == 0f && dir.Y > 0f)
-                platform = player.CollideFirst<Platform>(player.Position + dir);
-            else
-                platform = player.CollideFirst<Solid>(player.Position + dir);
-
-            if (platform != null)
-                player.LiftSpeed = platform.LiftSpeed;
-        }
-
+    
         private void Player_Jump(On.Celeste.Player.orig_Jump jump, Player player, bool particles, bool playsfx) {
-            if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection))
-                CheckForLiftboost(player, Vector2.UnitY);
-
+            if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection) 
+                && player.LiftSpeed == Vector2.Zero && TryGetPlatform(player, Vector2.UnitY, out var platform))
+                player.LiftSpeed = DynamicData.For(platform).Get<Vector2?>("safeLiftSpeed") ?? Vector2.Zero;
+        
             jump(player, particles, playsfx);
         }
-
+    
         private void Player_SuperJump(On.Celeste.Player.orig_SuperJump superJump, Player player) {
-            if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection))
-                CheckForLiftboost(player, Vector2.UnitY);
-
+            if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection)
+                && player.LiftSpeed == Vector2.Zero && TryGetPlatform(player, Vector2.UnitY, out var platform))
+                player.LiftSpeed = DynamicData.For(platform).Get<Vector2?>("safeLiftSpeed") ?? Vector2.Zero;
+        
             superJump(player);
         }
 
+        private void Player_orig_WallJump_il(ILContext il) {
+            var cursor = new ILCursor(il);
+
+            cursor.GotoNext(instr => instr.MatchCall<Actor>("set_LiftSpeed"));
+
+            cursor.Emit(OpCodes.Ldloc_2);
+            cursor.Emit(OpCodes.Ldarg_1);
+            cursor.EmitDelegate<Func<Vector2, Solid, int, Vector2>>((value, solid, dir) => {
+                if (!GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection))
+                    return value;
+        
+                var safeLiftSpeed = DynamicData.For(solid).Get<Vector2?>("safeLiftSpeed") ?? Vector2.Zero;
+                
+                if (Math.Sign(safeLiftSpeed.X) == dir)
+                    return safeLiftSpeed.X * Vector2.UnitX;
+
+                return value;
+            });
+        }
+    
         private void Player_SuperWallJump(On.Celeste.Player.orig_SuperWallJump superWallJump, Player player, int dir) {
-            if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection))
-                CheckForLiftboost(player, -5 * dir * Vector2.UnitX);
+            if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection)
+                && player.LiftSpeed == Vector2.Zero && TryGetPlatform(player, -5 * dir * Vector2.UnitX, out var platform)) {
+                var safeLiftSpeed = DynamicData.For(platform).Get<Vector2?>("safeLiftSpeed") ?? Vector2.Zero;
+            
+                if (Math.Sign(safeLiftSpeed.X) == dir)
+                    player.LiftSpeed = safeLiftSpeed.X * Vector2.UnitX;
+            }
 
             superWallJump(player, dir);
         }
 
-        private void Platform_Update_il(ILContext il) {
-            var cursor = new ILCursor(il);
+        private void Platform_Update(On.Celeste.Platform.orig_Update update, Platform platform) {
+            if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection))
+                DynamicData.For(platform).Set("safeLiftSpeed", Vector2.Zero);
 
-            cursor.Emit(OpCodes.Ldarg_0);
-            cursor.EmitDelegate<Action<Platform>>(platform => {
-                if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection))
-                    platform.LiftSpeed = Vector2.Zero;
-            });
-
-            cursor.GotoNext(instr => instr.MatchStfld<Platform>("LiftSpeed"));
-
-            cursor.Emit(OpCodes.Ldarg_0);
-            cursor.EmitDelegate<Func<Vector2, Platform, Vector2>>((value, platform)
-                => GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection) ? platform.LiftSpeed : value);
+            update(platform);
         }
 
         private void PatchLiftboostProtectionX(ILContext il) {
@@ -106,11 +128,21 @@ namespace ExtendedVariants.Variants {
                 cursor.GotoNext(instr => instr.MatchStfld<Vector2>("X"));
 
                 cursor.Emit(OpCodes.Ldarg_0);
-                cursor.EmitDelegate<Func<float, Platform, float>>((value, platform)
-                    => GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection) && value == 0f ? platform.LiftSpeed.X : value);
+                cursor.EmitDelegate<Func<float, Platform, float>>((value, platform) => {
+                    if (!GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection) || value == 0f)
+                        return value;
+        
+                    var dynamicData = DynamicData.For(platform);
+                    var safeLiftSpeed = dynamicData.Get<Vector2?>("safeLiftSpeed") ?? Vector2.Zero;
+
+                    safeLiftSpeed.X = value;
+                    dynamicData.Set("safeLiftSpeed", safeLiftSpeed);
+
+                    return value;
+                });
             }
         }
-
+    
         private void PatchLiftboostProtectionY(ILContext il) {
             var cursor = new ILCursor(il);
 
@@ -118,8 +150,18 @@ namespace ExtendedVariants.Variants {
                 cursor.GotoNext(instr => instr.MatchStfld<Vector2>("Y"));
 
                 cursor.Emit(OpCodes.Ldarg_0);
-                cursor.EmitDelegate<Func<float, Platform, float>>((value, platform)
-                    => GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection) && value == 0f ? platform.LiftSpeed.Y : value);
+                cursor.EmitDelegate<Func<float, Platform, float>>((value, platform) => {
+                    if (!GetVariantValue<bool>(ExtendedVariantsModule.Variant.LiftboostProtection) || value == 0f)
+                        return value;
+        
+                    var dynamicData = DynamicData.For(platform);
+                    var safeLiftSpeed = dynamicData.Get<Vector2?>("safeLiftSpeed") ?? Vector2.Zero;
+
+                    safeLiftSpeed.Y = value;
+                    dynamicData.Set("safeLiftSpeed", safeLiftSpeed);
+
+                    return value;
+                });
             }
         }
     }
