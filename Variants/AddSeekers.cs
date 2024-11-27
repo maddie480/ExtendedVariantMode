0e6edf3697ecd8d27800d2da272991ac6b4d8ee9
Maddie <52103563+maddie480@users.noreply.github.com>
2024-11-27 17:02:52 +0100

    Add Seekers variant: Make sure to place seekers in accessible areas
    aka Pathfinder Fun (tm)

--- Variants/AddSeekers.cs
+++ Variants/AddSeekers.cs
@@ -3,9 +3,12 @@ using Celeste.Mod;
 using ExtendedVariants.Entities;
 using ExtendedVariants.Module;
 using Microsoft.Xna.Framework;
+using Mono.Cecil.Cil;
 using Monocle;
 using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using static ExtendedVariants.Module.ExtendedVariantsModule;
 
@@ -15,6 +18,10 @@ namespace ExtendedVariants.Variants {
         private Random randomGenerator = new Random();
 
         private bool killSeekerSlowdownToFixHeart = false;
+        private string calcLogPrefix = null;
+        private bool pathfindingForVariant => calcLogPrefix != null;
+
+        private ILHook pathfinderFindHook = null;
 
         public override Type GetVariantType() {
             return typeof(int);
@@ -36,12 +43,19 @@ namespace ExtendedVariants.Variants {
             On.Celeste.Level.LoadLevel += modLoadLevel;
             IL.Celeste.SeekerEffectsController.Update += onSeekerEffectsControllerUpdate;
             On.Celeste.HeartGem.Collect += onHeartGemCollect;
+            On.Monocle.Calc.Log_ObjectArray += onCalcLog;
+
+            pathfinderFindHook = new ILHook(typeof(Pathfinder).GetMethod("orig_Find"), hookPathfinderFind);
         }
 
         public override void Unload() {
             On.Celeste.Level.LoadLevel -= modLoadLevel;
             IL.Celeste.SeekerEffectsController.Update -= onSeekerEffectsControllerUpdate;
             On.Celeste.HeartGem.Collect -= onHeartGemCollect;
+            On.Monocle.Calc.Log_ObjectArray -= onCalcLog;
+
+            pathfinderFindHook?.Dispose();
+            pathfinderFindHook = null;
 
             killSeekerSlowdownToFixHeart = false;
         }
@@ -56,12 +70,31 @@ namespace ExtendedVariants.Variants {
             Player player = level.Tracker.GetEntity<Player>();
 
             if (player != null && GetVariantValue<int>(Variant.AddSeekers) != 0) {
+                Rectangle maxBounds = new Rectangle(
+                    (int) Math.Floor(level.Bounds.X / 8f) * 8,
+                    (int) Math.Floor(level.Bounds.Y / 8f) * 8,
+                    (int) Math.Floor(level.Bounds.Width / 8f) * 8,
+                    (int) Math.Floor(level.Bounds.Height / 8f) * 8
+                );
+
+                Vector2 clampedPlayerPosition = new Vector2(
+                    Calc.Clamp(player.Center.X, maxBounds.Left, maxBounds.Right - 1),
+                    Calc.Clamp(player.Center.Y, maxBounds.Top, maxBounds.Bottom - 1)
+                );
+
+                Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", $"Level boundaries floored to multiples of 8 are: {maxBounds}");
+                Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", $"Player position was clamped from {player.Center} to {clampedPlayerPosition}");
+
+                List<Vector2> nullref = null;
+
                 // make the seeker barriers temporarily collidable so that they are taken in account in Solid collide checks
                 // and seekers can't spawn in them
                 // (... yes, this is also what vanilla does in the seekers' Update method.)
                 foreach (Entity entity in self.Tracker.GetEntities<SeekerBarrier>()) entity.Collidable = true;
 
                 for (int seekerCount = 0; seekerCount < GetVariantValue<int>(Variant.AddSeekers); seekerCount++) {
+                    bool found = false;
+
                     for (int i = 0; i < 100; i++) {
                         // roll a seeker position in the room
                         int x = randomGenerator.Next(level.Bounds.Width) + level.Bounds.X;
@@ -69,18 +102,36 @@ namespace ExtendedVariants.Variants {
 
                         // should be at least 100 pixels from the player
                         double playerDistance = Math.Sqrt(Math.Pow(MathHelper.Distance(x, player.X), 2) + Math.Pow(MathHelper.Distance(y, player.Y), 2));
-
-                        // also check if we are not spawning in a wall, that would be a shame
-                        Rectangle collideRectangle = new Rectangle(x - 8, y - 8, 16, 16);
-                        if (playerDistance > 100 && !level.CollideCheck<Solid>(collideRectangle) && !level.CollideCheck<Seeker>(collideRectangle)) {
-                            // build a Seeker with a proper EntityID to make Speedrun Tool happy (this is useless in vanilla Celeste but the constructor call is intercepted by Speedrun Tool)
-                            EntityData seekerData = ExtendedVariantsModule.GenerateBasicEntityData(level, 10 + seekerCount);
-                            seekerData.Position = new Vector2(x, y);
-                            Seeker seeker = new AutoDestroyingSeeker(seekerData, Vector2.Zero);
-                            level.Add(seeker);
-                            break;
+                        if (playerDistance <= 100) {
+                            Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", $"Rerolling seeker: position ({x}, {y}) too close from player ({playerDistance})");
+                            continue;
                         }
+
+                        // should not be spawning in a wall, that would be a shame
+                        Solid solid;
+                        if ((solid = level.CollideFirst<Solid>(new Rectangle(x - 8, y - 8, 16, 16))) != null) {
+                            Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", $"Rerolling seeker: position ({x}, {y}) is inside {solid.GetType().FullName}");
+                            continue;
+                        }
+
+                        // make sure we spawn the seeker in a zone accessible to the player (not "out of bounds")
+                        calcLogPrefix = $"Rerolling seeker: pathfinding to position ({x}, {y}) ";
+                        bool pathFound = level.Pathfinder.Find(ref nullref, from: clampedPlayerPosition, to: new Vector2(x, y), fewerTurns: false, logging: true);
+                        calcLogPrefix = null;
+                        if (!pathFound) continue;
+
+                        Logger.Log(LogLevel.Debug, "ExtendedVariantMode/AddSeekers", $"Spawning seeker: position ({x}, {y})");
+
+                        // build a Seeker with a proper EntityID to make Speedrun Tool happy (this is useless in vanilla Celeste but the constructor call is intercepted by Speedrun Tool)
+                        EntityData seekerData = ExtendedVariantsModule.GenerateBasicEntityData(level, 10 + seekerCount);
+                        seekerData.Position = new Vector2(x, y);
+                        Seeker seeker = new AutoDestroyingSeeker(seekerData, Vector2.Zero);
+                        level.Add(seeker);
+                        found = true;
+                        break;
                     }
+
+                    if (!found) Logger.Log(LogLevel.Warn, "ExtendedVariantMode/AddSeekers", "Bailed out after 100 rerolls without spawning a seeker!");
                 }
 
                 foreach (Entity entity in self.Tracker.GetEntities<SeekerBarrier>()) entity.Collidable = false;
@@ -115,6 +166,57 @@ namespace ExtendedVariants.Variants {
             // prevent seekers from slowing down time!
             if (self.Scene.Entities.OfType<AutoDestroyingSeeker>().Count() != 0) {
                 killSeekerSlowdownToFixHeart = true;
+            }
+        }
+
+        private void onCalcLog(On.Monocle.Calc.orig_Log_ObjectArray orig, object[] obj) {
+            orig(obj);
+
+            if (calcLogPrefix != null) {
+                foreach (object o in obj) {
+                    // all logs are prefixed with "PF: " which we can replace with our own prefix
+                    Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", calcLogPrefix + o.ToString().Substring(4));
+                }
+            }
+        }
+
+        private void hookPathfinderFind(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            if (cursor.TryGotoNext(instr => instr.MatchLdfld<Entity>("Collidable"))) {
+                Logger.Log("ExtendedVariantMode/AddSeekers", $"Marking dream blocks as traversable at {cursor.Index} in CIL code for Pathfinder.orig_Find");
+                cursor.Emit(OpCodes.Dup);
+                cursor.Index++;
+                cursor.EmitDelegate<Func<Entity, bool, bool>>((entity, orig) => {
+                    if (!pathfindingForVariant || (!(entity is DreamBlock) && !(entity is SeekerBarrier))) {
+                        return orig;
+                    }
+                    Level level = (Engine.Scene as Level) ?? (Engine.Scene as LevelLoader)?.Level;
+                    if (level == null) {
+                        Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", $"Not ignoring {entity.GetType().FullName} because there is no level to be found...?");
+                        return orig;
+                    }
+                    if (entity is DreamBlock && !level.Session.Inventory.DreamDash) {
+                        Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", $"Not ignoring {entity.GetType().FullName} because the player doesn't have the dream dash");
+                        return orig;
+                    }
+                    Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", $"Ignoring {entity.GetType().FullName} for pathfinding!");
+                    return false;
+                });
+            }
+
+            if (cursor.TryGotoNext(MoveType.AfterLabel, instr => instr.MatchLdarg(1))) {
+                Logger.Log("ExtendedVariantMode/AddSeekers", $"Adding early return at {cursor.Index} in CIL code for Pathfinder.orig_Find");
+
+                // if we are currently pathfinding for the variant, skip the "rebuild the found path" part of the pathfinder,
+                // since we only care about the path existing in the first place.
+                cursor.EmitDelegate<Func<bool>>(() => {
+                    if (pathfindingForVariant) Logger.Log(LogLevel.Verbose, "ExtendedVariantMode/AddSeekers", "Pathfinding is a SUCCESS");
+                    return pathfindingForVariant;
+                });
+                cursor.Emit(OpCodes.Brfalse, cursor.Next);
+                cursor.Emit(OpCodes.Ldc_I4_1);
+                cursor.Emit(OpCodes.Ret);
             }
         }
     }
