75bbe00b2c7f813df8212a29ef6d25070d87e3f3
SnipUndercover <snipundercover@gmail.com>
2025-07-14 00:21:12 +0200

    Fix formatting

--- Variants/ConsistentThrowing.cs
+++ Variants/ConsistentThrowing.cs
@@ -22,8 +22,7 @@ public class ConsistentThrowing : AbstractExtendedVariant {
     }
 
     private void MakeHoldableOffsetConsistent(On.Celeste.Holdable.orig_Release orig, Holdable self, Vector2 force) {
-        if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.ConsistentThrowing))
-        {
+        if (GetVariantValue<bool>(ExtendedVariantsModule.Variant.ConsistentThrowing)) {
             Player holder = self.Holder;
 
             Vector2 holdablePosition = holder.Position
@@ -31,8 +30,7 @@ public class ConsistentThrowing : AbstractExtendedVariant {
                 + Vector2.UnitY * (holder.Sprite.CarryYOffset / holder.Sprite.Scale.Y);
             self.Carry(holdablePosition.Floor());
 
-            if (self.Entity is Actor actor)
-            {
+            if (self.Entity is Actor actor) {
                 actor.ZeroRemainderX();
                 actor.ZeroRemainderY();
             }
