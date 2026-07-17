1aaf7a9bdb3177f855e91d9bcb88b79b59eda657
Maddie <52103563+maddie480@users.noreply.github.com>
2026-07-17 18:58:37 +0200

    Block calls to WallJump when Disable Wall Jumping is active

--- Variants/DisableWallJumping.cs
+++ Variants/DisableWallJumping.cs
@@ -1,18 +1,9 @@
 ﻿using Celeste;
-using Celeste.Mod;
-using ExtendedVariants.Module;
-using Mono.Cecil.Cil;
-using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
-using System;
-using System.Reflection;
 using static ExtendedVariants.Module.ExtendedVariantsModule;
 
 namespace ExtendedVariants.Variants {
     public class DisableWallJumping : AbstractExtendedVariant {
-
-        private static ILHook wallJumpHook;
-
         public DisableWallJumping() : base(variantType: typeof(bool), defaultVariantValue: false) { }
 
         public override object ConvertLegacyVariantValue(int value) {
@@ -20,49 +11,25 @@ namespace ExtendedVariants.Variants {
         }
 
         public override void Load() {
-            wallJumpHook = new ILHook(typeof(Player).GetMethod("orig_WallJump", BindingFlags.Instance | BindingFlags.NonPublic), modWallJump);
+            using (new DetourConfigContext(new DetourConfig("ExtendedVariantMode_AfterAll").WithPriority(int.MaxValue)).Use()) {
+                On.Celeste.Player.WallJump += onWallJump;
+            }
             On.Celeste.Player.WallJumpCheck += modWallJumpCheck;
         }
 
         public override void Unload() {
-            if (wallJumpHook != null) wallJumpHook.Dispose();
+            On.Celeste.Player.WallJump -= onWallJump;
             On.Celeste.Player.WallJumpCheck -= modWallJumpCheck;
         }
 
-        /// <summary>
-        /// Detour the WallJump method in order to disable it if we want.
-        /// </summary>
-        /// <param name="orig">the original method</param>
-        /// <param name="self">the player</param>
-        /// <param name="dir">the wall jump direction</param>
-        private static void modWallJump(ILContext il) {
-            ILCursor cursor = new ILCursor(il);
-
-            Instruction firstInstruction = cursor.Next;
-
-            Logger.Log("ExtendedVariantMode/DisableWallJumping", $"Injecting code to break method if wall jumping is disabled at {cursor.Index} in IL code for WallJump");
-
-            cursor.EmitDelegate<Func<bool>>(isWallJumpingDisabled);
-            cursor.Emit(OpCodes.Brfalse, firstInstruction);
-            cursor.Emit(OpCodes.Ret);
+        private static void onWallJump(On.Celeste.Player.orig_WallJump orig, Player self, int dir) {
+            if (GetVariantValue<bool>(Variant.DisableWallJumping)) return;
+            orig(self, dir);
         }
 
-        /// <summary>
-        /// Mods the WallJumpCheck method, checking if a walljump is possible.
-        /// </summary>
-        /// <param name="orig"></param>
-        /// <param name="self">the player</param>
-        /// <param name="dir">the direction</param>
-        /// <returns>true if walljumping is possible, false otherwise</returns>
         private static bool modWallJumpCheck(On.Celeste.Player.orig_WallJumpCheck orig, Player self, int dir) {
-            if (GetVariantValue<bool>(Variant.DisableWallJumping)) {
-                return false;
-            }
+            if (GetVariantValue<bool>(Variant.DisableWallJumping)) return false;
             return orig(self, dir);
-        }
-
-        private static bool isWallJumpingDisabled() {
-            return GetVariantValue<bool>(Variant.DisableWallJumping);
         }
     }
 }
